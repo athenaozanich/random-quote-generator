9d5216c0362723675aced6a5c4e827fe14955b46
athenaozanich <athenaozanich@gmail.com>
2018-10-24 15:02:29 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// FSJS - Random Quote Generator
+
+// Create the array of quote objects and name it quotes
+
+
+
+// Create the getRandomQuuote function and name it getRandomQuote
+
+
+
+// Create the printQuote funtion and name it printQuote
+
+
+
+// This event listener will respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
